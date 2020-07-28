c5a1d3d39f89284147891023d4aac990b76bca6e
nikitavolkovserg <nikitavolkovserg@gmail.com>
2020-07-29 03:28:35 +0700

    Big Update

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,7 @@ $('#year').text(new Date().getFullYear());
 $('body').scrollspy({target: '#main-nav'});
 
 // Smooth Scrolling
-$("#main-nav a").on('click', function (event) {
+$("#navbar a").on('click', function (event) {
     if (this.hash !== "") {
         event.preventDefault();
 
@@ -17,5 +17,33 @@ $("#main-nav a").on('click', function (event) {
 
         window.location.hash = hash;
         });
+    }
+});
+
+const special = document.querySelector('#special button');
+const html = document.querySelector('html');
+let isChanged = false;
+
+special.addEventListener('click', function() {
+    if (isChanged) {
+        location.reload();
+        isChanged = false;
+    }
+    else {
+        isChanged = true;
+        html.style.fontSize = "18px";
+        document.querySelectorAll(".lead, .card-text, td").forEach(function(element) {
+            element.style.fontWeight = "500";
+        });
+        document.querySelector("#header-section .overlay").style.backgroundColor = "rgba(0, 0, 0, 0.97)";
+        document.querySelector("main").style.background = "#fafafa";
+        document.querySelector("#navbar").style.boxShadow = "none";
+        document.querySelector("#navbar").style.borderBottom = "2px solid #000000";
+        document.querySelector("#more-reviews").className = "btn btn-outline-dark btn-lg";
+        document.querySelector("#more-reviews").style.border = "2px solid #000000";
+        document.querySelectorAll(".border-info").forEach(function(element) {
+            element.style.setProperty("border", "2px solid #000000", "important");
+        });
+        document.querySelector("footer").className = "bg-dark py-1 text-white";
     }
 });
